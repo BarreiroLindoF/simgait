ffc3ea9bda8b7794d39635946dab016c430c0e05
BarreiroLindoF <flavio_slb4@hotmail.com>
2019-12-06 17:34:43 +0100

    Ajout extraction de fichiers de forme normalisée

--- models/utils.py
+++ models/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 
 def load_csv(file_name, dtype='float', delimiter=",", skiprows=1):
     return np.loadtxt(open(file_name, "rb"), dtype=dtype, delimiter=delimiter, skiprows=skiprows)
@@ -22,3 +23,35 @@ def save_csv(file_name, data, first_row=None, delimiter=", "):
             else:
                 file_.write(str(row))
             skip_enter = False
+
+def convert_to_one_hot(label):
+    if np.equal(label, np.array([0, 0, 0, 0], dtype=float)).all():
+        return np.array[1, 0, 0, 0, 0, 0]
+    if np.equal(label, np.array([1, 0, 0, 0], dtype=float)).all():
+        return np.array([0, 1, 0, 0, 0, 0])
+    if np.equal(label, np.array([0, 1, 0, 0], dtype=float)).all():
+        return np.array([0, 0, 1, 0, 0, 0])
+    if np.equal(label, np.array([1, 1, 0, 0], dtype=float)).all():
+        return np.array([0, 0, 0, 1, 0, 0])
+    if np.equal(label, np.array([0, 0, 1, 0], dtype=float)).all():
+        return np.array([0, 0, 0, 0, 1, 0])
+    if np.equal(label, np.array([0, 0, 0, 1], dtype=float)).all():
+        return np.array([0, 0, 0, 0, 0, 1])
+
+    raise Exception
+
+def one_hot_to_text(one_hot):
+    if one_hot == np.array[1, 0, 0, 0, 0, 0]:
+        return "Normaux"
+    if one_hot == np.array([0, 1, 0, 0, 0, 0]):
+        return "CP_hemiplegia_left"
+    if one_hot == np.array([0, 0, 1, 0, 0, 0]):
+        return "CP_hemiplegia_right"
+    if one_hot == np.array([0, 0, 0, 1, 0, 0]):
+        return "CP_diplegia"
+    if one_hot == np.array([0, 0, 0, 0, 1, 0]):
+        return "ITW_triceps"
+    if one_hot == np.array([0, 0, 0, 0, 0, 1]):
+        return "ITW_soleus_triceps"
+
+    raise Exception
