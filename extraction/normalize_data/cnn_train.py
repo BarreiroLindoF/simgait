d1e835736ffea9e29dab0f9cfb6cca7909643a6a
BarreiroLindoF <flavio_slb4@hotmail.com>
2019-12-09 13:31:09 +0100

    Added new loss

--- extraction/normalize_data/cnn_train.py
+++ extraction/normalize_data/cnn_train.py
@@ -2,9 +2,10 @@ import numpy as np
 from utils import *
 import cv2
 
-files_train_csv = "D:\\Users\\Flavio\\Documents\\Research Project\\gait\\data\\extracted\\cross_val\\CV_4\\files_train.csv"
-y_train_csv = "D:\\Users\\Flavio\\Documents\\Research Project\\gait\\data\\extracted\\cross_val\\CV_4\\y_train.csv"
-markers_train = "D:\\Users\\Flavio\\Documents\\Research Project\\gait\\data\\extracted\\cross_val\\CV_4\\markers\\train\\"
+files_train_csv = "D:\\Users\\Flavio\\Documents\\Research Project\\gait\\data\\extracted\\cross_val\\CV_2\\files_train.csv"
+y_train_csv = "D:\\Users\\Flavio\\Documents\\Research Project\\gait\\data\\extracted\\cross_val\\CV_2\\y_train.csv"
+markers_train = "D:\\Users\\Flavio\\Documents\\Research Project\\gait\\data\\extracted\\cross_val\\CV_2\\markers\\train\\"
+angles_train = "D:\\Users\\Flavio\\Documents\\Research Project\\gait\\data\\extracted\\cross_val\\CV_2\\markers\\train\\"
 
 files_train_csv = load_csv(files_train_csv, dtype="U100", skiprows=0)
 y_train_csv = load_csv(y_train_csv, skiprows=0)
@@ -18,11 +19,16 @@ for i in range(files_train_csv.shape[0]):
         # print("Before formatting", data[0, :5, 0])
 
         data = np.vstack((np.vstack((data[:, :, 0], data[:, :, 1])), data[:, :, 2]))
+
+        angles = np.load(angles_train + file)
+        angles = np.vstack((np.vstack((angles[:, :, 0], angles[:, :, 1])), angles[:, :, 2]))
+
+        data = np.vstack((data, angles))
         # print(data.shape)
         # print("After formatting:", data[0, :5])
         # print(data.shape)
         # dsize = (width, height)
-        res = cv2.resize(data, dsize=(100, data.shape[0]), interpolation=cv2.INTER_CUBIC)
+        res = cv2.resize(data, dsize=(200, data.shape[0]), interpolation=cv2.INTER_CUBIC)
         res = res[np.newaxis, :, :]
 
         label = convert_to_one_hot(y_train_csv[i])
@@ -61,9 +67,10 @@ np.save("mean_train", mean)
 np.save("std_train", std)
 
 
-files_test_csv = "D:\\Users\\Flavio\\Documents\\Research Project\\gait\\data\\extracted\\cross_val\\CV_4\\files_test.csv"
-y_test_csv = "D:\\Users\\Flavio\\Documents\\Research Project\\gait\\data\\extracted\\cross_val\\CV_4\\y_test.csv"
-markers_test = "D:\\Users\\Flavio\\Documents\\Research Project\\gait\\data\\extracted\\cross_val\\CV_4\\markers\\test\\"
+files_test_csv = "D:\\Users\\Flavio\\Documents\\Research Project\\gait\\data\\extracted\\cross_val\\CV_2\\files_test.csv"
+y_test_csv = "D:\\Users\\Flavio\\Documents\\Research Project\\gait\\data\\extracted\\cross_val\\CV_2\\y_test.csv"
+markers_test = "D:\\Users\\Flavio\\Documents\\Research Project\\gait\\data\\extracted\\cross_val\\CV_2\\markers\\test\\"
+angles_test = "D:\\Users\\Flavio\\Documents\\Research Project\\gait\\data\\extracted\\cross_val\\CV_2\\markers\\test\\"
 
 files_test_csv = load_csv(files_test_csv, dtype="U100", skiprows=0)
 y_test_csv = load_csv(y_test_csv, skiprows=0)
@@ -77,11 +84,15 @@ for i in range(files_test_csv.shape[0]):
         # print("Before formatting", data[0, :5, 0])
 
         data = np.vstack((np.vstack((data[:, :, 0], data[:, :, 1])), data[:, :, 2]))
+        angles = np.load(angles_test + file)
+        angles = np.vstack((np.vstack((angles[:, :, 0], angles[:, :, 1])), angles[:, :, 2]))
+
+        data = np.vstack((data, angles))
         # print(data.shape)
         # print("After formatting:", data[0, :5])
         # print(data.shape)
         # dsize = (width, height)
-        res = cv2.resize(data, dsize=(100, data.shape[0]), interpolation=cv2.INTER_CUBIC)
+        res = cv2.resize(data, dsize=(200, data.shape[0]), interpolation=cv2.INTER_CUBIC)
         res = res[np.newaxis, :, :]
 
         label = convert_to_one_hot(y_test_csv[i])
